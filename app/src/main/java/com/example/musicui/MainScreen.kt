fbd5927ebe7d9fb0134fe4972b98f2402b205d4c
KOOLUNKLE\a4336 <a43361904@gmail.com>
2025-05-18 00:43:36 +0900

    Adding Resource Icons and Preparing DrawerItems in the Drawer

--- app/src/main/java/com/example/musicui/MainScreen.kt
+++ app/src/main/java/com/example/musicui/MainScreen.kt
@@ -1,5 +1,9 @@
 package com.example.musicui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -7,6 +11,7 @@ import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -14,6 +19,9 @@ import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -44,6 +52,32 @@ fun MainScreen() {
         Text(
             text = "Text",
             modifier = Modifier.padding(paddingValues)
+        )
+    }
+}
+
+@Composable
+fun DrawerItem(
+    selected: Boolean,
+    item: Screen.DrawerScreen,
+    onDrawerItemClicked: () -> Unit,
+) {
+    val color = if (selected) Color.DarkGray else Color.White
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 16.dp)
+            .background(color = color)
+            .clickable { onDrawerItemClicked() }
+    ) {
+        Icon(
+            painter = painterResource(id = item.icon),
+            contentDescription = item.drawerTitle,
+            modifier = Modifier.padding(top = 4.dp, end = 8.dp)
+        )
+        Text(
+            text = item.drawerTitle,
+            style = MaterialTheme.typography.bodyMedium,
         )
     }
 }
---
+++ app/src/main/java/com/example/musicui/Screen.kt
@@ -0,0 +1,32 @@
+package com.example.musicui
+
+import androidx.annotation.DrawableRes
+
+sealed class Screen(
+    val title: String,
+    val route: String,
+) {
+    sealed class DrawerScreen(
+        val drawerTitle: String,
+        val drawerRoute: String,
+        @DrawableRes val icon: Int,
+    ) : Screen(drawerTitle, drawerRoute) {
+        data object Account : DrawerScreen(
+            drawerTitle = "Account",
+            drawerRoute = "account",
+            icon = R.drawable.account,
+        )
+
+        data object Subscription : DrawerScreen(
+            drawerTitle = "Subscription",
+            drawerRoute = "subscription",
+            icon = R.drawable.subscribe,
+        )
+
+        data object AddAccount : DrawerScreen(
+            drawerTitle = "Add Account",
+            drawerRoute = "add_account",
+            icon = R.drawable.person_add_alt_1_24,
+        )
+    }
+}
